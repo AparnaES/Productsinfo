78eecdab81549472fe5b8258e908b6d90f47b0d7
unknown <aparnasomzz@gmail.com>
2023-02-21 18:32:52 +0530

    last commit

--- product.js
+++ product.js
@@ -1,5 +1,5 @@
 function addProduct() {
-    p={Name:pname.value,id:pid.value,price:prate.value,stock:pstock.value}
+    p={Name:pname.value,id:pid.value,price:prate.value,brand:pbrand.value,stock:pstock.value}
     if (pid.value in localStorage) {
         alert(`Product Already Exist..`)
     } else {
@@ -17,9 +17,49 @@ function searchProducts() {
         output.innerHTML=`<h3>Product ID : ${item.id}</h3>
         <h3>Product Name : ${item.Name}</h3>
         <h3>Product Price : ${item.price}</h3>
+        <h3>Product Brand : ${item.brand}</h3>
         <h3>Product Stockvalue : ${item.stock}</h3>`
     } else {
         output.innerHTML=`<h3>--Product Not Found--</h3>`
         alert("Product is not Present")
     }
+}
+function register() {
+    username=run.value
+    userid=ruid.value
+    password=rpw.value
+    phone=rnum.value
+
+    userdetails={username,userid,password,phone}
+    if (userid in localStorage) {
+        alert(`User already registered`)
+        window.location="index.html"
+    } else {
+        localStorage.setItem(userid, JSON.stringify(userdetails))
+        alert(`User Registered Succesfully`)
+        window.location="index.html"
+    }
+}
+function logIn() {
+    id=uid.value
+    psw=pw.value
+    if (id in localStorage) {
+        data=JSON.parse(localStorage.getItem(id))
+        if (psw==data["password"]) {
+            window.location="productinfo.html"
+            alert(`Login Successful`)
+
+        }
+         else {
+            alert(`Incorrect Password`)
+        }
+    } 
+    else {
+        alert(`Incorrect Username`)
+    }
+   
+}
+function logOut() {
+    localStorage.removeItem("id")
+    window.location="index.html"
 }
